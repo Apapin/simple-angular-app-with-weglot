da35cb28f390b726d4f98c11e6ca1ef0fc9e78bb
Alexandre Papin <me@alexandrepapin.name>
2022-05-13 16:27:25 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'customer-app-angular-example';
+  title = 'My homepage';
 }
